7debd2851180333bcf2949dffa3dd5cdc841f87e
marta-mazur <marta.mazur@ie.ibm.com>
2022-02-25 13:41:35 +0000

    Exposing and customizing openapi docs

--- mlserver/handlers/openapi_schema.py
+++ mlserver/handlers/openapi_schema.py
@@ -1,33 +1,29 @@
-import yaml
 import re
+import yaml
 
 
 def get_schema():
 
-    with open('api_docs/openapi.yaml') as f:
-        schema = yaml.load(f, Loader=yaml.FullLoader)
+    schema = merge_schemas('openapi/dataplane.yaml', 'openapi/model_repository.yaml')
+    endpoints = []
 
-        endpoints = []
+    # get API paths
+    for path in schema['paths']:
+        path_node = schema['paths'][path]
+        # operations supported by openapi 3.0
+        operations = ["get", "post", "put", "patch", "delete", "head", "options", "trace"]
+        for operation in path_node:
+            if operation in operations:
+                endpoint = {"path": normalize_paths(path), "operation": operation}
+                print(endpoint["path"])
+                if 'description' in schema['paths'][path][operation]:
+                    endpoint["desc"] = schema['paths'][path][operation]['description']
+                if 'summary' in schema['paths'][path][operation]:
+                    endpoint["summary"] = schema['paths'][path][operation]['summary']
 
-        # get API paths
-        for path in schema['paths']:
-            path_node = schema['paths'][path]
-            # operations supported by openapi 3.0
-            operations = ["get", "post", "put", "patch", "delete", "head", "options", "trace"]
-            for node in path_node:
-                if node in operations:
-                    endpoint = {}
-                    operation = node
-                    description = schema['paths'][path][operation]['description']
-                    summary = schema['paths'][path][operation]['summary']
-                    endpoint["path"] = normalize_paths(path)
-                    print(endpoint["path"])
-                    endpoint["operation"] = operation
-                    endpoint["desc"] = description
-                    endpoint["summary"] = summary
-                    endpoints.append(endpoint)
+                endpoints.append(endpoint)
 
-        return endpoints
+    return endpoints
 
 
 def normalize_paths(path: str):
@@ -37,10 +33,26 @@ def normalize_paths(path: str):
                      {"to_replace": r'\$\{MODEL_VERSION\}',
                       "replacement": "{model_version}"},
                      {"to_replace": r'/v2/$',
-                      "replacement": "/v2"},
-                                        ]
+                      "replacement": "/v2"}]
+
     for element in path_elements:
         path = re.sub(element["to_replace"], element["replacement"], path)
 
     return path
 
+
+def merge_schemas(path_1, path_2):
+    with open(path_1, encoding='utf-8') as file:
+        schema_1 = yaml.load(file, Loader=yaml.FullLoader)
+
+    with open(path_2, encoding='utf-8') as file:
+        schema_2 = yaml.load(file, Loader=yaml.FullLoader)
+
+    merged_schema = schema_1.copy()
+    merged_schema['paths'].update(schema_2.get('paths', {}))
+    merged_schema['components']['schemas'].update(schema_2['components'].get('schemas', {}))
+
+    with open('openapi/toyaml.yaml', 'w', encoding='utf-8') as file:
+        yaml.dump(merged_schema, file, sort_keys=False)
+
+    return merged_schema
