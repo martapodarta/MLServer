a601c4fc0f11beeb769795c7064794eb4341ebcc
marta-mazur <marta.mazur@ie.ibm.com>
2022-03-04 13:35:24 +0000

    Adding unit tests

--- tests/rest/test_openapi_schema.py
+++ tests/rest/test_openapi_schema.py
@@ -13,7 +13,7 @@ def test_updated_dataplane(updated_schema):
     Test if an updated summary and description for '/v2/health/live' endpoint is returned by FastAPI
     """
     assert updated_schema['paths']['/v2/health/live']['get']['description'] == "The “server live” API indicates if the inference server is able to receive and respond to metadata and inference requests."
-    assert updated_schema['paths']['/v2/health/live']['get']['summary'] == "Server Live"
+    assert updated_schema['paths']['/v2/health/live']['get']['summary'] == "Server is live"
 
 
 def test_add_new_endpoint(updated_schema):
