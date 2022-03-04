f809f3af9eadf8eb116071041fca1cfe75f3ab3e
marta-mazur <marta.mazur@ie.ibm.com>
2022-03-04 13:35:24 +0000

    Adding unit tests

--- mlserver/handlers/openapi_schema.py
+++ mlserver/handlers/openapi_schema.py
@@ -3,11 +3,11 @@ from typing import List, Dict
 import yaml
 
 
-
-def process_schema(input_schema) -> List[Dict[str, str]]:
+def process_schema(input_schema: Dict) -> List[Dict[str, str]]:
     """
     Method used to extract API paths and corresponding descriptions and summaries.
-    Returns list of object, where each object includes an API path, operation and
+    Takes in as a parameter openapi schema, represented as a dictionary object.
+    Returns list of objects, where each object includes an API path, operation and
     if available summary and a description of an API
     """
 
@@ -21,7 +21,6 @@ def process_schema(input_schema) -> List[Dict[str, str]]:
         for operation in path_node:
             if operation in operations:
                 endpoint = {"path": normalize_paths(path), "operation": operation}
-                #print(endpoint["path"])
                 if 'description' in input_schema['paths'][path][operation]:
                     endpoint["desc"] = input_schema['paths'][path][operation]['description']
                 if 'summary' in input_schema['paths'][path][operation]:
@@ -34,7 +33,7 @@ def process_schema(input_schema) -> List[Dict[str, str]]:
 
 def normalize_paths(path: str):
     """
-        Method used to normalize API paths to match MLServer API paths
+        Method used to normalize API paths in yaml files to match MLServer API paths
     """
     path_elements = [{"to_replace": r'\$\{MODEL_NAME\}',
                       "replacement": "{model_name}"},
@@ -49,7 +48,12 @@ def normalize_paths(path: str):
     return path
 
 
-def merge_schemas(path_1, path_2):
+def merge_schemas(path_1: str, path_2: str) -> Dict[str, str]:
+    """
+        Method used to merge API paths and schemas of two openapi yaml files.
+        The paths are taken in as parameters.
+        It returns a dictionary of merged schemas.
+    """
     with open(path_1, encoding='utf-8') as file:
         schema_1 = yaml.load(file, Loader=yaml.FullLoader)
 
@@ -57,8 +61,10 @@ def merge_schemas(path_1, path_2):
         schema_2 = yaml.load(file, Loader=yaml.FullLoader)
 
     merged_schema = schema_1.copy()
-    merged_schema['paths'].update(schema_2.get('paths', {}))
-    merged_schema['components']['schemas'].update(schema_2['components'].get('schemas', {}))
+    merged_schema['paths'].update(schema_2['paths'])
+    merged_schema['components']['schemas'].update(schema_2['components']['schemas'])
+
+    with open('openapi/toyaml.yaml', 'w', encoding='utf-8') as file:
+        yaml.dump(merged_schema, file, sort_keys=False)
 
     return merged_schema
-
