4b1bcd5894a3b05b72fbca4f86085c63d99c652a
marta-mazur <marta.mazur@ie.ibm.com>
2022-03-24 17:47:39 +0100

    Normalizing schemas

--- mlserver/handlers/__init__.py
+++ mlserver/handlers/__init__.py
@@ -1,13 +1,12 @@
 from .dataplane import DataPlane
 from .model_repository import ModelRepositoryHandlers
 from .custom import get_custom_handlers, custom_handler
-from .openapi_schema import process_schema, merge_schemas
+from .openapi_schema import merge_schemas
 
 __all__ = [
     "DataPlane",
     "ModelRepositoryHandlers",
     "get_custom_handlers",
     "custom_handler",
-    "process_schema",
     "merge_schemas"
 ]
--- mlserver/handlers/openapi_schema.py
+++ mlserver/handlers/openapi_schema.py
@@ -5,55 +5,49 @@ to retrieve descriptions and summaries of endpoints
 import re
 from typing import List, Dict
 import yaml
+import json
 
 
-def process_schema(input_schema: Dict) -> List[Dict[str, str]]:
-    """
-    Method used to extract API paths and corresponding descriptions and summaries.
-    Takes in as a parameter openapi schema, represented as a dictionary object.
-    Returns list of objects, where each object includes an API path, operation and
-    if available summary and a description of an API
-    """
+def normalize_schema(schema: Dict):
 
-    endpoints = []
-
-    # get API paths
-    for path in input_schema['paths']:
-        path_node = input_schema['paths'][path]
-        # operations supported by openapi 3.0
-        operations = ["get", "post", "put", "patch",
-                      "delete", "head", "options", "trace"]
-        for operation in path_node:
-            if operation in operations:
-                endpoint = {"path": normalize_paths(path),
-                            "operation": operation}
-                if 'description' in input_schema['paths'][path][operation]:
-                    endpoint["desc"] = \
-                        input_schema['paths'][path][operation]['description']
-                if 'summary' in input_schema['paths'][path][operation]:
-                    endpoint["summary"] = \
-                        input_schema['paths'][path][operation]['summary']
-
-                endpoints.append(endpoint)
-
-    return endpoints
-
-
-def normalize_paths(path: str):
-    """
-        Method used to normalize API paths in yaml files to match MLServer API paths
-    """
-    path_elements = [{"to_replace": r'\$\{MODEL_NAME\}',
-                      "replacement": "{model_name}"},
-                     {"to_replace": r'\$\{MODEL_VERSION\}',
-                      "replacement": "{model_version}"},
-                     {"to_replace": r'/v2/$',
-                      "replacement": "/v2"}]
-
+    path_elements = [{"to_replace": r'\$\{MODEL_NAME\}', "replacement": "{model_name}"},
+                     {"to_replace": r'\$\{MODEL_VERSION\}', "replacement": "{model_version}"},
+                     {"to_replace": r'/v2/$', "replacement": "/v2"}]
+    # normalize paths
     for element in path_elements:
-        path = re.sub(element["to_replace"], element["replacement"], path)
+        for path in list(schema['paths'].keys()):
+            if 'parameters' in schema['paths'][path]:
+                #for i in schema['paths'][path]['parameters']:
+                print(schema['paths'][path]['parameters'])
+            #path = re.sub(element["to_replace"], element["replacement"], path
+            schema['paths'][re.sub(element["to_replace"], element["replacement"], path)] = schema['paths'].pop(path)
+   # print(schema['paths'][path]['parameters'])
 
-    return path
+
+    # normalize schema keys
+    schemas = {}
+    for schema_comp in list(schema['components']['schemas'].keys()):
+
+        #schema['components']['schemas'][schema_comp.title().replace("_", "")] = schema['components']['schemas'][schema_comp].pop(schema_comp)
+        key = schema_comp.title().replace("_", "")
+        schemas[key] = schema_comp
+        schema['components']['schemas'][key] = schema['components']['schemas'].pop(schema_comp)
+        schema['components']['schemas'][key]['title'] = key
+
+
+    new_schema = json.dumps(schema)
+
+    for key, value in schemas.items():
+        to_replace = '#/components/schemas/' + value
+        replacement = '#/components/schemas/' + key
+
+        new_schema = new_schema.replace(to_replace, replacement)
+
+   # print("inside normalize")
+   # print(new_schema)
+
+    return new_schema
+
 
 
 def merge_schemas(path_1: str, path_2: str) -> Dict[str, str]:
@@ -62,14 +56,26 @@ def merge_schemas(path_1: str, path_2: str) -> Dict[str, str]:
         The paths are taken in as parameters.
         It returns a dictionary of merged schemas.
     """
-    with open(path_1, encoding='utf-8') as file:
+    #TODO handle normalization here
+    with open(path_1) as file:
         schema_1 = yaml.load(file, Loader=yaml.FullLoader)
+    schema_1 = json.loads(normalize_schema(schema_1))
+    #print("inside merge1")
+    #print(schema_1)
 
-    with open(path_2, encoding='utf-8') as file:
+    with open(path_2) as file:
         schema_2 = yaml.load(file, Loader=yaml.FullLoader)
+    schema_2 = json.loads(normalize_schema(schema_2))
+    #print("inside merge2")
+    #print(schema_2)
+
+
 
     merged_schema = schema_1.copy()
     merged_schema['paths'].update(schema_2['paths'])
     merged_schema['components']['schemas'].update(schema_2['components']['schemas'])
+
+    with open('openapi/toyaml.yaml', 'w', encoding='utf-8') as file:
+        yaml.dump(merged_schema, file, sort_keys=False)
 
     return merged_schema
